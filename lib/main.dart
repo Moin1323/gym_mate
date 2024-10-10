7ab645768519a72cd5744cab3eb94804964cbe01
Moin <121077008+Moin1323@users.noreply.github.com>
2024-10-11 01:12:42 +0500

    FIREBASE | configuration done

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ import 'package:gym_mate/res/getx_localization/languages.dart';
 import 'package:gym_mate/res/routes/app_routes.dart';
 import 'package:gym_mate/res/theme/app_theme.dart';
 import 'package:gym_mate/view/splash/splash_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
